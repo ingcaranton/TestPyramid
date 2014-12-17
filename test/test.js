8cb96a97c1c42f99f23e70d6eb9f1a5f2311b0c9
ingcaranton <ing.caranton@gmail.com>
2014-12-17 16:13:51 -0500

    ejemplo pruebas unitarias

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
 var chai = require("chai");
 var expect = chai.expect;
+
 describe("unitTest", function() {
-   	describe('Array', function(){
-	  	describe('#indexOf()', function(){
-		   	it('should return -1 when the value is not present', function(){
-		      	chai.assert.equal(-1, [1,2,3].indexOf(5));
-		      	chai.assert.equal(-1, [1,2,3].indexOf(0));
+   	describe('Validarse', function(){
+	  	describe('#validar()', function(){
+		   	it('comprueba si se pudo valdiar en BD', function(){
+		      	chai.assert.equal("", "");
 		    });
 		});
 	}),
-   	describe("Cadenas",function(){
-		describe("subcadenas",function(){
-		   it('deberia retornar una sub cadenas',function(){
-		   		expect("Bello").to.equal("Panama es Bello".substring(10,15));
+   	describe("GuardarDatos",function(){
+		describe("#guardar()",function(){
+		   it('comprueba si esta guardando los datos en la BD',function(){
+		   		expect("").to.equal("");
 		   });
 		}) 
 	});
